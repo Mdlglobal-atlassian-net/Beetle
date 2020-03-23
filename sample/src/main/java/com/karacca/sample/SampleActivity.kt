f99e2f4837eedfc10e6431c8c6261c11c5e6debc
Ömer Faruk Karaca <karacca@Omers-MacBook-Pro.local>
2020-03-23 12:31:28 +0300

    initial commit

---
+++ sample/src/main/java/com/karacca/sample/SampleActivity.kt
@@ -0,0 +1,17 @@
+package com.karacca.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @user: omer.karaca
+ * @date: 23.03.2020
+ */
+
+class SampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
---
+++ sample/src/main/java/com/karacca/sample/SampleApplication.kt
@@ -0,0 +1,10 @@
+package com.karacca.sample
+
+import android.app.Application
+
+/**
+ * @user: omer.karaca
+ * @date: 23.03.2020
+ */
+
+class SampleApplication : Application()
